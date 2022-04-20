cb8839d690f17567a180d642479c54f902704da5
James Eren <erenjamesjohnson@gmail.com>
2022-04-20 02:24:56 +0100

    Ozer commit

---
+++ src/ErenIBILI.java
@@ -0,0 +1,9 @@
+public class ErenIBILI {
+    public static void main(String[] args) {
+        System.out.println("Erenn deli oglan");
+
+        // Ozer notes
+        System.out.println("Ozer");
+
+    }
+}
